be523a4a1aaaf6b8ed90cd70eebef9cfa432cdd3
root <ahmedsherifakl.asa@gmail.com>
2025-09-05 22:35:31 +0000

    fix ip adapter gen 2

--- degis/__init__.py
+++ degis/__init__.py
@@ -8,7 +8,7 @@ and various visual features (color histograms, edge maps, etc.).
 from .core.embeddings import generate_clip_embeddings, generate_xl_embeddings
 from .core.features import generate_color_histograms, generate_edge_maps
 from .core.training import train_color_model, train_edge_model
-from .core.generation import IPAdapterGenerator, IPAdapterXLGenerator, generate_from_embeddings, load_trained_color_head, get_color_embedding
+from .core.generation import IPAdapterGenerator, IPAdapterXLGenerator, generate_from_embeddings, load_trained_color_head, get_color_embedding, create_edge_control_image
 from .core.visualization import (
     plot_color_palette, display_images_grid, display_comparison_grid,
     extract_top_palette, visualize_histogram_comparison, plot_training_metrics,
@@ -39,4 +39,5 @@ __all__ = [
     "plot_training_metrics",
     "save_generation_results",
     "create_side_by_side_comparison",
+    "create_edge_control_image",
 ]
