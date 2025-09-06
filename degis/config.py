d85d932c4aaf4769d3c1208a345ec820bc75165e
root <ahmedsherifakl.asa@gmail.com>
2025-09-06 23:25:49 +0000

    run all notebooks

--- degis/config.py
+++ degis/config.py
@@ -15,20 +15,19 @@ os.environ["HF_HOME"] = MODEL_CACHE
 NAME = "test"
 DATA_DIR = "/data/thesis/data"
 MODELS_DIR = "/data/thesis/models"
-EDGE_MAPS_PATH = f"{DATA_DIR}/{NAME}_edge_maps.npy"
-HF_HUB_CACHE = f"{DATA_DIR}/models"
-CSV_PATH = f"/data/thesis/{NAME}_manifest.csv"
+EDGE_MAPS_PATH = f"/data/thesis/data/laion_5m_edge_maps.npy"
+CSV_PATH = f"/data/thesis/laion_5m_manifest.csv"
 
-EMBEDDINGS_PATH = f"{DATA_DIR}/{NAME}_clip_embeddings.npy"
-EMBEDDINGS_TARGET_PATH = f"{MODELS_DIR}/{NAME}_embeddings.npy"
+EMBEDDINGS_PATH = f"/data/thesis/data/laion_5m_clip_embeddings.npy"
+EMBEDDINGS_TARGET_PATH = f"/data/thesis/models/laion_5m_embeddings.npy"
 
-HF_XL_EMBEDDINGS_TARGET_PATH = f"{MODELS_DIR}/hf_xl_{NAME}_embeddings.npy"
-
+HF_XL_EMBEDDINGS_TARGET_PATH = f"/data/thesis/models/hf_xl_laion_5m_embeddings.npy"
 
 
-COLOR_HIST_PATH_RGB = f"{DATA_DIR}/{NAME}_color_histograms_rgb_512.npy"
-COLOR_HIST_PATH_LAB_514 = f"{DATA_DIR}/{NAME}_color_histograms_lab_514.npy"
-COLOR_HIST_PATH_HCL_514 = f"{DATA_DIR}/{NAME}_color_histograms_hcl_514.npy"
+
+COLOR_HIST_PATH_RGB = f"/data/thesis/data/laion_5m_color_histograms_rgb_512.npy"
+COLOR_HIST_PATH_LAB_514 = f"/data/thesis/data/laion_5m_color_histograms_lab_514.npy"
+COLOR_HIST_PATH_HCL_514 = f"/data/thesis/data/laion_5m_color_histograms_hcl_514.npy"
 
 
 
