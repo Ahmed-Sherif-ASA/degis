16c9a106fa1de015e78175f3df55d58395bc73ec
root <ahmedsherifakl.asa@gmail.com>
2025-09-12 23:30:47 +0000

    comment not initialised code

--- degis_clean/__init__.py
+++ degis_clean/__init__.py
@@ -16,20 +16,20 @@ __version__ = "2.0.0"
 __author__ = "DEGIS Team"
 
 # Import main functionality for easy access
-from .shared.config import *
+# from .shared.config import *
 from .data.dataset import UnifiedImageDataset
-from .shared.embeddings import generate_clip_embeddings, generate_xl_embeddings
-from .inference.generation import IPAdapterGenerator, IPAdapterXLGenerator
-from .inference.features import generate_color_histograms, generate_edge_maps
-from .training.training import train_color_model
+# from .shared.embeddings import generate_clip_embeddings, generate_xl_embeddings
+# from .inference.generation import IPAdapterGenerator, IPAdapterXLGenerator
+# from .inference.features import generate_color_histograms, generate_edge_maps
+# from .training.training import train_color_model
 
 __all__ = [
     "UnifiedImageDataset",
-    "generate_clip_embeddings", 
-    "generate_xl_embeddings",
-    "IPAdapterGenerator",
-    "IPAdapterXLGenerator", 
-    "generate_color_histograms",
-    "generate_edge_maps",
-    "train_color_model"
+    # "generate_clip_embeddings", 
+    # "generate_xl_embeddings",
+    # "IPAdapterGenerator",
+    # "IPAdapterXLGenerator", 
+    # "generate_color_histograms",
+    # "generate_edge_maps",
+    # "train_color_model"
 ]
--- degis_clean/shared/__init__.py
+++ degis_clean/shared/__init__.py
@@ -5,11 +5,11 @@ Contains functionality used by both training and inference modules.
 """
 
 from .config import *
-from .embeddings import generate_clip_embeddings, generate_xl_embeddings
-from .utils import create_control_edge_pil
+# from .embeddings import generate_clip_embeddings, generate_xl_embeddings
+# from .utils import create_control_edge_pil
 
 __all__ = [
-    "generate_clip_embeddings",
-    "generate_xl_embeddings", 
-    "create_control_edge_pil"
+    # "generate_clip_embeddings",
+    # "generate_xl_embeddings", 
+    # "create_control_edge_pil"
 ]
