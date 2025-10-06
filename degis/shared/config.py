902c736e16745db832be00a0482588fc9518b87a
root <ahmedsherifakl.asa@gmail.com>
2025-10-06 21:45:19 +0000

    use local images

--- degis/shared/config.py
+++ degis/shared/config.py
@@ -12,26 +12,20 @@ os.environ["HF_HOME"] = MODEL_CACHE
 # Dataset paths
 # NAME = "laion_5m"
 # NAME = "coco"
+# NAME = "adimagenet"
 NAME = "demo"
-DATA_DIR = "/data/thesis/data"
-MODELS_DIR = "/data/thesis/models"
+DATA_DIR = "data"
 EDGE_MAPS_PATH = f"{DATA_DIR}/{NAME}_edge_maps.npy"
-CSV_PATH = f"/data/thesis/{NAME}_manifest.csv"
+CSV_PATH = f"{DATA_DIR}/{NAME}_manifest.csv"
 
 EMBEDDINGS_PATH = f"{DATA_DIR}/{NAME}_clip_embeddings.npy"
-EMBEDDINGS_TARGET_PATH = f"{MODELS_DIR}/{NAME}_embeddings.npy"
+EMBEDDINGS_TARGET_PATH = f"{DATA_DIR}/{NAME}_embeddings.npy"
 
-HF_XL_EMBEDDINGS_TARGET_PATH = f"{MODELS_DIR}/hf_xl_{NAME}_embeddings.npy"
-
-
+HF_XL_EMBEDDINGS_TARGET_PATH = f"{DATA_DIR}/hf_xl_{NAME}_embeddings.npy"
 
 COLOR_HIST_PATH_RGB = f"{DATA_DIR}/{NAME}_color_histograms_rgb_512.npy"
 COLOR_HIST_PATH_LAB_514 = f"{DATA_DIR}/{NAME}_color_histograms_lab_514.npy"
 COLOR_HIST_PATH_HCL_514 = f"{DATA_DIR}/{NAME}_color_histograms_hcl_514.npy"
-
-
-
-
 
 BATCH_SIZE = 512
 IMG_SIZE = (224,224)
